70ab28bf7390b6d1e8f09f2d94d6c92075e24550
Dan Jang <yeh2@ada.cs.pdx.edu>
2023-02-01 16:46:20 -0800

    Project1 main class comments

--- airline/src/main/java/edu/pdx/cs410J/yeh2/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/yeh2/Project1.java
@@ -4,8 +4,19 @@ import com.google.common.annotations.VisibleForTesting;
 
 /**
  * The main class for the CS410J airline Project
- * Creates an airline, lufthansa (can be named differently internally of course), that creates a flight(s), then prints all flight(s) information to console.
- * Note: Project is massively incomplete, will most likely re-submit for regrading, my apologies in advance to the wonderful grader.
+ * usage: java -jar target/airline-2023.0.0.jar [options] <args>
+ *      args are (in this order):
+ *          airline      The name of the airline
+ *          flightNumber     The flight number
+ *          src      Three-letter code of departure airport
+ *          depart      Departure date and time (24-hour time)
+ *          dest     Three-letter code of arrival airtport
+ *          arrive      Arrival date and time (24-hour time)
+ *      options are (options may appear in any order))
+ *          -print      Prints a description of the new flight
+ *          -README     Prints a README for this project and exits
+ *      Date and time should be in the format: mm/dd/yyyy hh:mm
+ *
  */
 public class Project1 {
 
