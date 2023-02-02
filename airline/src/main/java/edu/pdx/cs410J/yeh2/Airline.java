7d001194bce951613bf0d9d67137ad2751fca174
Dan Jang <yeh2@ada.cs.pdx.edu>
2023-02-01 17:10:05 -0800

    whoops, scrolled down to implement destination & arrival get functions & javadocs

--- airline/src/main/java/edu/pdx/cs410J/yeh2/Airline.java
+++ airline/src/main/java/edu/pdx/cs410J/yeh2/Airline.java
@@ -4,6 +4,10 @@ import edu.pdx.cs410J.AbstractAirline;
 
 import java.util.Collection;
 
+
+/**
+ * The airline class has (should have collection) a linked list of Flight classes.
+ */
 public class Airline extends AbstractAirline<Flight> {
   private String name = "N/A";
   private Flight head = null;
@@ -23,11 +27,19 @@ public class Airline extends AbstractAirline<Flight> {
     this.flights = 0;
   }
 
+  /**
+   * Returns the name of the airline.
+   * @return name
+   */
   @Override
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Adds a flight to the linked list (should be collection)
+   *
+   */
   @Override
   public void addFlight(Flight curr) {
     if (this.head == null)
@@ -44,7 +56,9 @@ public class Airline extends AbstractAirline<Flight> {
       this.addFlight(curr.getNext());
     }
   }
-
+/**
+ * Prints all flights.
+ */
   public void printAll()
   {
     if (this.head == null)
@@ -59,8 +73,24 @@ public class Airline extends AbstractAirline<Flight> {
     }
   }
 
+  /**
+   * Should return the flights but currently prints all the flights.
+   *
+   */
   @Override
   public Collection<Flight> getFlights() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    if (this.head == null)
+    {
+      System.out.print("\nDone! (or no flights)");
+    }
+    else
+    {
+      head.print();
+      this.head = head.getNext();
+      printAll();
+    }
+
+    return null;
+    //throw new UnsupportedOperationException("This method is not implemented yet");
   }
 }
