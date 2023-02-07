121d2cf9db1ee3bb6934b2b4cd6a3631d1b78834
Dan Jang <yeh2@ada.cs.pdx.edu>
2023-02-07 08:43:49 -0800

    project #2 - 11hr49min mark - progress checkpoint R.) - getting very very very very close

--- airline/src/test/java/edu/pdx/cs410J/yeh2/TextParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/yeh2/TextParserTest.java
@@ -7,6 +7,7 @@ import java.io.*;
 //import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
@@ -59,6 +60,10 @@ public class TextParserTest {
 //    }
     }
 
+    /**
+     * valid text file test.
+     * @throws ParserException
+     */
     @Test
     void validTextFileCanBeParsed() throws ParserException {
         //InputStream resource = getClass().getResourceAsStream("valid-airline.txt");
@@ -78,6 +83,10 @@ public class TextParserTest {
         test_file.deleteOnExit();
     }
 
+    /**
+     * Invalid text file test.
+     * @throws ParserException
+     */
     @Test
     void invalidTextFileReturnsNull() throws ParserException {
         //InputStream resource = getClass().getResourceAsStream("empty-airline.txt");
@@ -86,5 +95,58 @@ public class TextParserTest {
         TextParser parser = new TextParser(this.testfile2);//new InputStreamReader(resource));
         //assertThrows(ParserException.class, parser::parse);
         assertThat(parser.parse(), equalTo(null));
+    }
+
+    /**
+     * invalid text file test, v2.
+     * @throws ParserException
+     * @throws IllegalArgumentException
+     */
+
+    @Test
+    void invalidTextFileCannotBeParsedv2() throws ParserException, IllegalArgumentException {
+        //InputStream resource = getClass().getResourceAsStream("valid-airline.txt");
+        //assertThat(testFilesPresent(this.testfile1), equalTo(true));
+        File test_file = new File("test.txt");
+        try (PrintWriter testwrite = new PrintWriter(test_file))
+        {
+            testwrite.println("Lufthansa, 123, 24M");
+            testwrite.println("            !!!!!!!!!!!!!!!!!!!!!!!!!!!!!9d0f2j30f0923j 023,f23,,,,,,,,,,,,,,,,,,,");
+        }
+        catch (FileNotFoundException t3)
+        {
+            throw new RuntimeException("Valid Test Text File was unable to be created: ", t3);
+        }
+        TextParser parser = new TextParser(test_file);//new InputStreamReader(resource));
+        //Airline airline = parser.parse();
+        test_file.deleteOnExit();
+        //assertThrows(IllegalArgumentException.class, parser::parse);
+        assertThrows(ParserException.class, parser::parse);
+    }
+
+    /**
+     * Invalid text file test, v3.
+     * @throws ParserException
+     * @throws IllegalArgumentException
+     */
+    @Test
+    void invalidTextFileCannotBeParsedv3() throws ParserException, IllegalArgumentException {
+        //InputStream resource = getClass().getResourceAsStream("valid-airline.txt");
+        //assertThat(testFilesPresent(this.testfile1), equalTo(true));
+        File test_file = new File("test.txt");
+        try (PrintWriter testwrite = new PrintWriter(test_file))
+        {
+            testwrite.println("Lufthansa, 123, PDX, 2/04/2023 6:51:34 AM, XDP, 2414, 34525322/0325234/3202523, 7:25235235:00322 AM, 351253, 4543");
+            testwrite.println("g4g43g43gg,43g,4g334g34,g,34g,34 g, 34 ,g3,4 g3, 34,g ,3g4, 3g, 253, 4543");
+        }
+        catch (FileNotFoundException t3)
+        {
+            throw new RuntimeException("Valid Test Text File was unable to be created: ", t3);
+        }
+        TextParser parser = new TextParser(test_file);//new InputStreamReader(resource));
+        //Airline airline = parser.parse();
+        test_file.deleteOnExit();
+        //assertThrows(IllegalArgumentException.class, parser::parse);
+        assertThrows(ParserException.class, parser::parse);
     }
 }
