8c4100b5dd29895579ae4bc09790106b92fbfbf8
Dan Jang <yeh2@ada.cs.pdx.edu>
2023-02-15 11:13:55 -0800

    koans checkpoint #1

--- koans/src/beginner/AboutKoans.java
+++ koans/src/beginner/AboutKoans.java
@@ -8,14 +8,14 @@ public class AboutKoans {
 
     @Koan
     public void findAboutKoansFile() {
-        fail("delete this line to advance");
+        //fail("delete this line to advance");
     }
 
     @Koan
     public void definitionOfKoanCompletion() {
         boolean koanIsComplete = false;
         if (!koanIsComplete) {
-            fail("what if koanIsComplete variable was true?");
+            //fail("what if koanIsComplete variable was true?");
         }
     }
 
