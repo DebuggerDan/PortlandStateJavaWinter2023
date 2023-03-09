da0a22f9f2db8d1e42829b646b6e5e50cab92acf
User name <68046886+DebuggerDan@users.noreply.github.com>
2023-03-09 04:17:00 -0800

    project #5 setup, II

--- airline-web/src/main/java/edu/pdx/cs410J/yeh2/AirlineServlet.java
+++ airline-web/src/main/java/edu/pdx/cs410J/yeh2/AirlineServlet.java
@@ -28,155 +28,214 @@ import java.util.Map;
  *
  */
 public class AirlineServlet extends HttpServlet {
-  static final String WORD_PARAMETER = "word";
-  static final String DEFINITION_PARAMETER = "definition";
+    static final String WORD_PARAMETER = "word";
+    //static final String DEFINITION_PARAMETER = "definition";
+    static final String AIRLINE_PARAMETER = "airline";
 
-  static final String AIRLINE_PARAMETER = "airline";
-  static final String FLIGHTNUMBER_PARAMETER = "flightNumber";
-  static final String SRC_PARAMETER = "src";
-  static final String DEPART_PARAMETER = "depart";
-  static final String DEST_PARAMETER = "dest";
-  static final String ARRIVE_PARAMETER = "arrive";
+    static final String FLIGHTNUMBER_PARAMETER = "flightNumber";
+    static final String SRC_PARAMETER = "src";
+    static final String DEPART_PARAMETER = "depart";
+    static final String DEST_PARAMETER = "dest";
+    static final String ARRIVE_PARAMETER = "arrive";
 
-  //private final Map<String, String> dictionary = new HashMap<>();
-  private final Map<String, Airline> dictionary = new HashMap<>();
+    //private final Map<String, String> dictionary = new HashMap<>();
+    private final Map<String, Airline> dictionary = new HashMap<>();
 
-  /**
-   * Handles an HTTP GET request from a client by writing the definition of the
-   * word specified in the "word" HTTP parameter to the HTTP response.  If the
-   * "word" parameter is not specified, all of the entries in the dictionary
-   * are written to the HTTP response.
-   */
-  @Override
-  protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws IOException
-  {
-      response.setContentType( "text/plain" );
+    /**
+     * Handles an HTTP GET request from a client by writing the definition of the
+     * word specified in the "word" HTTP parameter to the HTTP response.  If the
+     * "word" parameter is not specified, all entries in the dictionary
+     * are written to the HTTP response.
+     * @param request The HTTP request!
+     * @param response The HTTP response!
+     * @throws IOException If there is a problem writing the HTTP response!
+     */
+    @Override
+    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws IOException
+    {
+        response.setContentType( "text/plain" );
 
-      String word = getParameter( WORD_PARAMETER, request );
-      if (word != null) {
-          writeDefinition(word, response);
+        String word = getParameter( WORD_PARAMETER, request );
+        if (word != null) {
+            writeDefinition(word, response);
 
-      } else {
-          writeAllDictionaryEntries(response);
-      }
-  }
-
-  /**
-   * Handles an HTTP POST request by storing the dictionary entry for the
-   * "word" and "definition" request parameters.  It writes the dictionary
-   * entry to the HTTP response.
-   */
-  @Override
-  protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws IOException
-  {
-      response.setContentType( "text/plain" );
-
-      String word = getParameter(WORD_PARAMETER, request );
-      if (word == null) {
-          missingRequiredParameter(response, WORD_PARAMETER);
-          return;
-      }
-
-      String definition = getParameter(DEFINITION_PARAMETER, request );
-      if ( definition == null) {
-          missingRequiredParameter( response, DEFINITION_PARAMETER );
-          return;
-      }
-
-      this.dictionary.put(word, definition);
-
-      PrintWriter pw = response.getWriter();
-      pw.println(Messages.definedWordAs(word, definition));
-      pw.flush();
-
-      response.setStatus( HttpServletResponse.SC_OK);
-  }
-
-  /**
-   * Handles an HTTP DELETE request by removing all dictionary entries.  This
-   * behavior is exposed for testing purposes only.  It's probably not
-   * something that you'd want a real application to expose.
-   */
-  @Override
-  protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      response.setContentType("text/plain");
-
-      this.dictionary.clear();
-
-      PrintWriter pw = response.getWriter();
-      pw.println(Messages.allDictionaryEntriesDeleted());
-      pw.flush();
-
-      response.setStatus(HttpServletResponse.SC_OK);
-
-  }
-
-  /**
-   * Writes an error message about a missing parameter to the HTTP response.
-   *
-   * The text of the error message is created by {@link Messages#missingRequiredParameter(String)}
-   */
-  private void missingRequiredParameter( HttpServletResponse response, String parameterName )
-      throws IOException
-  {
-      String message = Messages.missingRequiredParameter(parameterName);
-      response.sendError(HttpServletResponse.SC_PRECONDITION_FAILED, message);
-  }
-
-  /**
-   * Writes the definition of the given word to the HTTP response.
-   *
-   * The text of the message is formatted with {@link TextDumper}
-   */
-  private void writeDefinition(String word, HttpServletResponse response) throws IOException {
-    String definition = this.dictionary.get(word);
-
-    if (definition == null) {
-      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-
-    } else {
-      PrintWriter pw = response.getWriter();
-
-      Map<String, String> wordDefinition = Map.of(word, definition);
-      TextDumper dumper = new TextDumper(pw);
-      dumper.dump(wordDefinition);
-
-      response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            writeAllDictionaryEntries(response);
+        }
     }
-  }
 
-  /**
-   * Writes all of the dictionary entries to the HTTP response.
-   *
-   * The text of the message is formatted with {@link TextDumper}
-   */
-  private void writeAllDictionaryEntries(HttpServletResponse response ) throws IOException
-  {
-      PrintWriter pw = response.getWriter();
-      TextDumper dumper = new TextDumper(pw);
-      dumper.dump(dictionary);
+    /**
+     * Handles an HTTP POST request by storing the dictionary entry for the
+     * "word" and "definition" request parameters.  It writes the dictionary
+     * entry to the HTTP response.
+     * @param request The HTTP request!
+     * @param response The HTTP response!
+     * @throws IOException If there is a problem writing the HTTP response!
+     */
+    @Override
+    protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws IOException
+    {
+        response.setContentType( "text/plain" );
 
-      response.setStatus( HttpServletResponse.SC_OK );
-  }
+        String word = getParameter(WORD_PARAMETER, request );
+        if (word == null)
+        {
+            missingRequiredParameter(response, WORD_PARAMETER);
+            return;
+        }
 
-  /**
-   * Returns the value of the HTTP request parameter with the given name.
-   *
-   * @return <code>null</code> if the value of the parameter is
-   *         <code>null</code> or is the empty string
-   */
-  private String getParameter(String name, HttpServletRequest request) {
-    String value = request.getParameter(name);
-    if (value == null || "".equals(value)) {
-      return null;
+        String airline = getParameter(AIRLINE_PARAMETER, request );
+        if ( airline == null)
+        {
+            missingRequiredParameter( response, AIRLINE_PARAMETER );
+            return;
+        }
 
-    } else {
-      return value;
+        String flightNumber = getParameter(FLIGHTNUMBER_PARAMETER, request );
+        if ( flightNumber == null)
+        {
+            missingRequiredParameter( response, FLIGHTNUMBER_PARAMETER );
+            return;
+        }
+
+        String src = getParameter(SRC_PARAMETER, request );
+        if ( src == null)
+        {
+            missingRequiredParameter( response, SRC_PARAMETER );
+            return;
+        }
+
+        String depart = getParameter(DEPART_PARAMETER, request );
+        if ( depart == null)
+        {
+            missingRequiredParameter( response, DEPART_PARAMETER );
+            return;
+        }
+
+        String dest = getParameter(DEST_PARAMETER, request );
+        if ( dest == null)
+        {
+            missingRequiredParameter( response, DEST_PARAMETER );
+            return;
+        }
+
+        String arrive = getParameter(ARRIVE_PARAMETER, request );
+        if ( arrive == null)
+        {
+            missingRequiredParameter( response, ARRIVE_PARAMETER );
+            return;
+        }
+
+        //this.dictionary.put(word, definition);
+        Airline lufthansa = this.dictionary.get(airline);
+
+        if (lufthansa == null)
+        {
+            lufthansa = new Airline(airline);
+            pw.println("New airline has been created with the following name: '" + airline + ".'");
+        }
+
+        PrintWriter pw = response.getWriter();
+        pw.println(Messages.definedWordAs(word, airline));
+        pw.flush();
+
+        response.setStatus( HttpServletResponse.SC_OK);
     }
-  }
 
-  @VisibleForTesting
-  String getDefinition(String word) {
-      return this.dictionary.get(word);
-  }
+    /**
+     * Handles an HTTP DELETE request by removing all dictionary entries.  This
+     * behavior is exposed for testing purposes only.  It's probably not
+     * something that you'd want a real application to expose.
+     * @param request The HTTP request!
+     * @param response The HTTP response!
+     * @throws IOException If there is an error writing to the HTTP response!
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+
+        this.dictionary.clear();
+
+        PrintWriter pw = response.getWriter();
+        pw.println(Messages.allDictionaryEntriesDeleted());
+        pw.flush();
+
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    /**
+     * Writes an error message about a missing parameter to the HTTP response.
+     *
+     * The text of the error message is created by {@link Messages#missingRequiredParameter(String)}
+     */
+    private void missingRequiredParameter( HttpServletResponse response, String parameterName )
+            throws IOException
+    {
+        String message = Messages.missingRequiredParameter(parameterName);
+        response.sendError(HttpServletResponse.SC_PRECONDITION_FAILED, message);
+    }
+
+    /**
+     * Writes the definition of the given word to the HTTP response.
+     * @param word The word that should have its definition(s) to the HTTP response!
+     * @param response The HTTP response to write the definition(s) to!
+     * @throws IOException If there is an error writing to the HTTP response!
+     * The text of the message is formatted with {@link TextDumper}
+     */
+    private void writeDefinition(String word, HttpServletResponse response) throws IOException {
+        Airline lufthansa = this.dictionary.get(word);
+
+        if (lufthansa == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Unable to find airline named: '" + word + "'!");
+
+        } else {
+            PrintWriter pw = response.getWriter();
+
+            Map<String, Airline> wordDefinition = Map.of(word, lufthansa);
+            TextDumper dumper = new TextDumper(pw);
+            dumper.dump(wordDefinition);
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+
+    /**
+     * Writes all dictionary entries to the HTTP response.
+     *
+     * @param response The HTTP response to write the dictionary entries to!
+     * @throws IOException If there is an error writing to the HTTP response!
+     * The text of the message is formatted with {@link TextDumper}
+     */
+    private void writeAllDictionaryEntries(HttpServletResponse response ) throws IOException
+    {
+        PrintWriter pw = response.getWriter();
+        TextDumper dumper = new TextDumper(pw);
+        dumper.dump(dictionary);
+
+        response.setStatus( HttpServletResponse.SC_OK );
+    }
+
+    /**
+     * Returns the value of the HTTP request parameter with the given name.
+     *
+     * @param name The parameter name!
+     * @param request The HTTP request source the parameterz from!
+     * @return <code>null</code> if the value of the parameter is
+     *         <code>null</code> or is the empty string
+     */
+    private String getParameter(String name, HttpServletRequest request) {
+        String value = request.getParameter(name);
+        if (value == null || "".equals(value)) {
+            return null;
+
+        } else {
+            return value;
+        }
+    }
+
+    @VisibleForTesting
+    String getDefinition(String word) {
+        return this.dictionary.get(word);
+    }
 }
---
+++ airline-web/src/test/java/edu/pdx/cs410J/yeh2/XmlDumperTest.java
@@ -0,0 +1,115 @@
+package edu.pdx.cs410J.yeh2;
+
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+class XmlDumperTest {
+
+        /**
+         * A function that reads (returns <code>String</code>s) txt files!
+         * It also deletes the file afterwards so that there are no pesky txt files cluttering the resource folders!
+         * @see TextDumperTest
+         * @param txtfile The text file name-string!
+         * @return result A string from a file that was read by the function!
+         * @throws IOException If the file cannot be read!
+         */
+        private String reader(String txtfile) throws IOException
+        {
+            StringBuilder result = new StringBuilder();
+            //result.append("");
+
+            File read_file = new File(txtfile);
+            FileReader file_read = new FileReader(read_file);
+
+            try (BufferedReader read_buffer = new BufferedReader(file_read))
+            {
+                String currline = read_buffer.readLine();
+
+                while (currline != null)
+                {
+                    result.append(currline);
+                    currline = read_buffer.readLine();
+
+                    if (currline != null)
+                    {
+                        result.append("\n");
+                    }
+                }
+            }
+            catch (IOException m1)
+            {
+                //System.out.println("Error! README not found!", m1);
+            }
+
+            File alright_time_to = new File(txtfile);
+            alright_time_to.delete();
+
+            return result.toString();
+        }
+
+        /**
+         * <code>XML</code>Dumper Test #1: Valid <code>XML</code> file checks!
+         * A test that tests valid <code>XML</code>-dumped files!.
+         * @throws IOException If there are file related errors!
+         * @throws ParseException If there are parsing related errors!
+         * @throws ParserConfigurationException If there are <code>XML</code>-specific parsing errors!
+         * @throws SAXException If there are <code>SAX</code>-<code>XML</code> API specific errors!
+         */
+        @Test
+    void canDumpValidAirline2XML() throws IOException, ParseException, ParserConfigurationException, SAXException
+    {
+        String filename = "dump_test.xml";
+        String airlineName = "Lufthansa";
+        String flightNumber = "123";
+        String src = "PDX";
+        String depart = "02/04/2023 6:51 am";
+        String dest = "SEA";
+        String arrive = "02/04/2023 7:00 am";
+
+        Flight testdrive = new Flight(flightNumber, src, depart, dest, arrive);
+        Airline airline = new Airline(airlineName, testdrive);
+
+        File file = new File(filename);
+
+        //StringWriter sw = new StringWriter();
+        XmlDumper dumper = new XmlDumper(filename);//sw.toString());
+        try {
+            dumper.dump(airline);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String text = reader(filename);//sw.toString();
+
+        assertThat(text, containsString("<!DOCTYPE airline SYSTEM \"http://www.cs.pdx.edu/~whitlock/dtds/airline.dtd\">"));// +
+        // "\n123, PDX, 02/04/2023 6:51 am, SEA, 02/04/2023 7:00 am"));
+        AirlineXmlHelper dtdTest = new AirlineXmlHelper();
+        DocumentBuilderFactory dtdTestFactory = DocumentBuilderFactory.newInstance();
+        dtdTestFactory.setValidating(true);
+
+        DocumentBuilder dtdTestBuilder = dtdTestFactory.newDocumentBuilder();
+        dtdTestBuilder.setErrorHandler(dtdTest);
+        dtdTestBuilder.setEntityResolver(dtdTest);
+        try (FileReader dtdTestReader = new FileReader(file))
+        {
+            dtdTestBuilder.parse(filename);
+        }
+        catch (IOException m1)
+        {
+           System.err.println("[XmlDumperTest #1 Error, IOException]" + m1.getMessage());
+        }
+
+    }
+}
